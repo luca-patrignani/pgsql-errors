00604f06fd4e61fd855d5d7763b9cb46928f9463
Luca Patrignani <luca0patrignani@gmail.com>
2025-12-05 00:28:22 +0100

    fix: use kotest for writing tests

--- src/test/kotlin/io/github/lucapatrignani/pgsqlerrors/TryTest.kt
+++ src/test/kotlin/io/github/lucapatrignani/pgsqlerrors/TryTest.kt
@@ -1,12 +1,12 @@
 package io.github.lucapatrignani.pgsqlerrors
 
+import io.kotest.core.spec.style.FunSpec
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
 
-class TryTest {
-
-    @Test
-    fun test() {
-        assertTrue(false)
+class TryTest : FunSpec() {
+    init {
+        test("try") {
+            assertTrue(true)
+        }
     }
 }
